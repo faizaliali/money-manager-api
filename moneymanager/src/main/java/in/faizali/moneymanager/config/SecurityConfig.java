503453d3141464a21094c8b5172219aa890f1d85
faizaliali <139729561+faizaliali@users.noreply.github.com>
2025-08-14 23:46:48 +0530

    Update SecurityConfig.java

--- moneymanager/src/main/java/in/faizali/moneymanager/config/SecurityConfig.java
+++ moneymanager/src/main/java/in/faizali/moneymanager/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        //configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedOriginPatterns(List.of("https://moneymanager-webapp.netlify.app"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
